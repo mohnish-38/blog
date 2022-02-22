ea4b3b8fc0ccd90fc74c141064c10ebec6e6d104
mohnish-38 <jharbadeji38@gmail.com>
2022-02-22 20:22:07 +0530

    added all the crud functionalities

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController
+    def index
+    @articles = Article.all
+    end
+
+    def show
+    @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            redirect_to article_path(@article)
+        else
+            render :new, status: :unprocessable_entity
+        
+        end
+    end 
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            redirect_to article_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+        
+    end
+
+    def destroy
+       @article = Article.find(params[:id])
+       @article.destroy
+       redirect_to articles_path, status: :see_other
+    end
+end
---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true, length: {minimum:5,maximum:200}
+end

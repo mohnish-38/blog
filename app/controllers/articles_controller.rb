8d9bbf2dc0168d3e8a446a5d704d86e772bed455
mohnish-38 <jharbadeji38@gmail.com>
2022-02-24 17:50:27 +0530

    Added one-to-many accociations btw users and articles from back end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.user = User.first
         if @article.save
             redirect_to article_path(@article)
         else

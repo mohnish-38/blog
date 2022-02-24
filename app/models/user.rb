c759f43bb82bc7e204509f00d882a81d3cf19c7f
mohnish-38 <jharbadeji38@gmail.com>
2022-02-24 23:10:31 +0530

    all fine

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-    before_save { self.email = email.downcase!}
     has_many :articles
     before_save {self.email = email.downcase!}
     validates :username, presence: true, 
@@ -8,8 +7,7 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, 
                         uniqueness: { case_sensitive: false},  
-                        length: {maximum: 105 },
-                        format: { with: VALID_EMAIL_REGEX }
-
-   has_secure_password
+                        length: {maximum:105},
+                        format: {with: VALID_EMAIL_REGEX }
+    has_secure_password
 end

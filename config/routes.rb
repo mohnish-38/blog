b6d78fc113b2c8925970cffcd4d0ce8a828e15bd
mohnish-38 <jharbadeji38@gmail.com>
2022-02-24 20:47:09 +0530

    just a commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "pages#home"
-  resources "articles" 
+  resources "articles"
+  get "signup", to: 'users#new'  
   
 end

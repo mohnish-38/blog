b808ff8e89bd7d3afa8016f221a63fb17105f849
mohnish-38 <jharbadeji38@gmail.com>
2022-02-25 15:35:42 +0530

    Added edit user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+    before_action :set_user , only: [:edit, :update]
     def new
         @user =User.new
     end
+
+    def edit
+        
+    end 
 
     def create
         @user = User.new(user_params)
@@ -12,10 +17,22 @@ class UsersController < ApplicationController
            render 'new' , status: :unprocessable_entity
         end 
     end
+
+    def update
+      if @user.update(user_params)
+        redirect_to articles_path
+      else
+        render :edit,  status: :unprocessable_entity
+      end
+    end
     
     private
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+
+    def set_user
+        @user = User.find(params[:id])
+    end    
 end

45f36a1cfdf56a7399042d580c823d66e9b1da55
mohnish-38 <jharbadeji38@gmail.com>
2022-02-28 20:10:14 +0530

    added login user page and functinalities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to the Blog, you have successfully signed up"
             redirect_to articles_path
         else
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   root "pages#home"
   resources "articles"
   get "signup", to: 'users#new'
-  resources :users, except: [:new]  
+  resources :users, except: [:new] 
+  get "login", to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   
 end
